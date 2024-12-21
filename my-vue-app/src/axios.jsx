370caf709c2e83eb61a6beeaac7623108fec65c5
veerpm <vmehra@ualberta.ca>
2024-12-21 12:29:15 -0700

    updated url

--- my-vue-app/src/axios.jsx
+++ my-vue-app/src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://api-gamerankd.onrender.com";
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "https://api-gamerankd.onrender.com";
+// axios.defaults.baseURL = "http://localhost:3000";
 
 axios.interceptors.response.use(
   (response) => response,
